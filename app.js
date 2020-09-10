20d5a7db30b9d30d9ab7dd4c60a86e0c35447bc2
lisambamberg <lisambamberg@gmail.com>
2020-09-10 15:24:57 -0500

    Refactor cellClicked function

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 let cells = document.querySelectorAll(".row > div");
 let player1 = "X";
 let player2 = "O";
+let gameBoard = [["", "", ""], ["", "", ""], ["", "", ""]];
+
+let currentTurn = 0;
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener("click", cellClicked);
 }
 
 function cellClicked() {
-    event.target.textContent = "X";
-}
-
-function playerTurn() {
-    
-}
-
-function winCombo() {
-
-}
-function resetBoard() {
-
+    if (event.target.textContent === "") {
+        if (currentTurn % 2 == 0) {
+            event.target.textContent = player1;
+        } else {
+            event.target.textContent = player2;
+        }
+        currentTurn++;
+    }
 }
