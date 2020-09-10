93463cbe26873a2a67e7aa49025a66a40ae6674e
lisambamberg <lisambamberg@gmail.com>
2020-09-10 13:42:59 -0500

    Updated functions

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 let cells = document.querySelectorAll(".row > div");
-let currentPlayer = "X";
+let player1 = "X";
+let player2 = "O";
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener("click", cellClicked);
 }
 
 function cellClicked() {
-    event.target.textContent = currentPlayer;
-    if (currentPlayer == "X") {
-        currentPlayer = "O";
-    } else {
-        currentPlayer = "X";
-    }
+    event.target.textContent = "X";
 }
 
+function playerTurn() {
+    
+}
+
+function winCombo() {
+
+}
+function resetBoard() {
+
+}
